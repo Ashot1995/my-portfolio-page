5e79726fa4dd701d2e53caf2e93d41208e081d4d
ashot_g <ashotgharakeshishyan@gmail.com>
2018-06-02 13:53:36 +0400

    don't open :)

--- assets/sendMail/send.php
+++ assets/sendMail/send.php
@@ -7,9 +7,9 @@
  */
 
 
-$name = $_POST["name"];
-$mail = $_POST["mail"];
-$desc = $_POST["description"];
+$name = $_GET["name"];
+$mail = $_GET["mail"];
+$desc = $_GET["description"];
 
 //$aa = fopen("newfile7.txt","a");
 //$txt =" ----".$name."- ---$mail-------$desc-\n";

98b1509a470d7b1c70052a01cc404adf45578eb6
ashot_g <ashotgharakeshishyan@gmail.com>
2018-06-06 12:05:06 +0400

    on't open :)

--- sendMail/send.php
+++ sendMail/send.php
@@ -1,25 +1,11 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: ashot
- * Date: 5/30/18
- * Time: 11:44 PM
- */
+$name=$_POST['name'];
+$email= $_POST['email'];
+$content = $_POST['content'];
 
-
-$name = $_POST["name"];
-$mail = $_POST["mail"];
-$desc = $_POST["description"];
-
-ini_set( 'display_errors', 1 );
-error_reporting( E_ALL );
-$from = ".'$mail.'";
-$to = "ashotgharakeshishyan@gmail.com";
-$subject = "$name";
-$message = "$desc";
-$headers = "From:" . $from;
-mail($to,$subject,$message, $headers);
-echo "The email message was sent.";
-header('Location: ../index.html');
-
-?>
+$to = 'ashotgharakeshishyan@gmail.com';
+$subject = $name;
+$message = $content;
+$headers = $email;
+mail($to, $subject, $message, $headers);
+header("Location: ../index.html");

1ede77502697791fd4d79b9ff502b7c08b2eb646
ashot_g <ashotgharakeshishyan@gmail.com>
2018-06-06 13:13:50 +0400

    Don't open :)

---
+++ assets/js/contact.js
@@ -0,0 +1,48 @@
+ jQuery(document).ready(function ($) {
+
+     $("#contact").on("click", function () {
+         $("#send").css("display", "flex");
+
+         $("#close").css('cursor', 'pointer');
+         $("#close").click(function () {
+             $("#send").css("display", "none");
+         })
+     })
+
+     $("#submit").on("click", function () {
+
+         let name = $("#name").val();
+         let mail = $("#mail").val();
+         let description = $("#content").val();
+
+         let pattern1 = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
+         // console.log(pattern1.test(mail))
+
+         if (pattern1.test(mail) === true) {
+              alert("ok")
+             $.ajax({
+                 url: "assets/plugins/contact.php",
+                 type: "GET",
+                 data: {
+                     'description': description,
+                     'mail': mail,
+                     'name': name
+
+                 },
+                 success: function () {
+
+                     alert("Your message sent");
+                 }
+
+             });
+             $("#send").css("display", "none");
+         } else {
+             $("#send").css("display", "flex");
+             $("#name").css("border", "1px solid red");
+             $("#mail").css("border", "1px solid red");
+             $("#content").css("border", "1px solid red");
+         }
+
+
+     });
+ })
